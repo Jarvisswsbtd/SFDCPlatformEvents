038f07197eeb48b013f0f79afcb81f8d7ede1881
Melissa Hansen <mhansen@stand.org>
2018-09-27 14:56:15 -0700

    Add files via upload

---
+++ src/aura/ViewPlatformEvents/ViewPlatformEventsHelper.js
@@ -0,0 +1,64 @@
+({
+    connectCometd : function(component) {
+        var helper = this;
+        // Configure CometD
+        var cometdUrl = window.location.protocol + '//' + window.location.hostname + '/cometd/40.0/';
+        var cometd = component.get('v.cometd');
+        cometd.configure({
+            url: cometdUrl,
+            requestHeaders: { Authorization: 'OAuth ' + component.get('v.sessionId') },
+                appendMessageTypeToURL: false
+            });
+        cometd.websocketEnabled = false;
+    
+        // Establish CometD connection
+        console.log('Connecting to CometD: ' + cometdUrl);
+        cometd.handshake(function(handshakeReply) {
+            if (handshakeReply.successful) {
+                console.log('Connected to CometD.');
+                // Subscribe to platform event
+                var newSubscription = cometd.subscribe('/event/Text_to_Lead__e',
+                    function(platformEvent) {
+                        console.log('Platform event received: ' + JSON.stringify(platformEvent));
+                        helper.onReceiveNotification(component, platformEvent);
+                    }
+                );
+                
+                // Save subscription for later
+                var subscriptions = component.get('v.cometdSubscriptions');
+                subscriptions.push(newSubscription);
+                component.set('v.cometdSubscriptions', subscriptions);
+            } else {
+                console.error('Failed to connected to CometD.');
+            }
+        });
+    },
+    disconnectCometd : function(component) {
+        var cometd = component.get('v.cometd');
+        // Unsuscribe all CometD subscriptions
+        cometd.batch(function() {
+            var subscriptions = component.get('v.cometdSubscriptions');
+            subscriptions.forEach(function (subscription) {
+                cometd.unsubscribe(subscription);
+            });
+        });
+        component.set('v.cometdSubscriptions', []);
+        // Disconnect CometD  
+        cometd.disconnect();
+        console.log('CometD disconnected.');
+    },
+    onReceiveNotification : function(component, platformEvent) {
+        var helper = this;
+        // Extract notification from platform event
+        var newNotification = {
+            time: $A.localizationService.formatDateTime(platformEvent.data.payload.CreatedDate, 'HH:mm'),
+            fName: platformEvent.data.payload.First_Name__c ,
+            lName: platformEvent.data.payload.Last_Name__c,
+            phone: platformEvent.data.payload.Mobile__c 
+        };
+        // Save notification in history
+        var notifications = component.get('v.pEvents');
+        notifications.push(newNotification);
+        component.set('v.pEvents', notifications);
+    }
+})
